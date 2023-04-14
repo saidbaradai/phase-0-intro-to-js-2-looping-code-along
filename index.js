286f8bb773d1872c284da75ae27f43d15d9ac73e
said baradai <saitbaradai@gmail.com>
2023-04-14 05:09:27 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Code your solutions in this file
+function writeCards(arr, word){
+    let output=[]
+
+    for (let i = 0; i < arr.length; i++) {
+        output.push(`Thank you, ${arr[i]}, for the wonderful ${word} gift!`)
+        
+    }
+    return output
+
+}
+
+function countDown(number){
+    for (let i = number; i >= 0; i--) {
+        console.log(i)
+        
+    }
+}
